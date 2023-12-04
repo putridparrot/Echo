d6f100a59c8332b784070dfa6249549e479e3d52
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2023-12-04 11:13:43 +0000

    Initial code

---
+++ Echo/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/echo", (string text) =>
+    {
+        return $"Echo: {text}";
+    })
+.WithName("Echo")
+.WithOpenApi();
+
+app.Run();
